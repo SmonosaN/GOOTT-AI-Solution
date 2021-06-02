bb9fa621c5f09580b661c0893c1521d835495fcc
bbaktaeho <lth9036@gmail.com>
2021-06-02 12:02:41 +0900

    패키지 설계

--- src/App.java
+++ src/App.java
@@ -1,16 +1,6 @@
 
 public class App {
 	public static void main(String[] args) {
-		System.out.print("test123");
-		System.out.print("test123");
-
-		System.out.print("test123");
-
-		System.out.print("test123");
-
-		System.out.print("test123");
-
-		System.out.print("test123");
-
+		
 	}
 }
