c055d827b6d15e2f9e3b8109f22040e369d973cb
bbaktaeho <lth9036@gmail.com>
2021-06-02 11:16:37 +0900

    태호가 메인함수 수정했음

--- src/App.java
+++ src/App.java
@@ -2,5 +2,15 @@
 public class App {
 	public static void main(String[] args) {
 		System.out.print("test123");
+		System.out.print("test123");
+
+		System.out.print("test123");
+
+		System.out.print("test123");
+
+		System.out.print("test123");
+
+		System.out.print("test123");
+
 	}
 }
