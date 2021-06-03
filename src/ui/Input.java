fb4a83ac088d008eb008fc2d2ad8a7b71e337562
SmonosaN <tjdals3741@naver.com>
2021-06-03 18:10:19 +0900

    정규표현식 1차

--- src/ui/Input.java
+++ src/ui/Input.java
@@ -16,12 +16,24 @@ public class Input {
 		}
 	}
 	
-	public static String inputString() {
+	public static String inputGroupString() {
 		try {
 			String input = INPUT.nextLine();
-			// todo: Á¤±ÔÇ¥Çö½ÄÀ¸·Î ideal, likely, defact È®ÀÎÇÏ±â
-			
+			// todo: Á¤±ÔÇ¥Çö½ÄÀ¸·Î ideal, likely, defeat È®ÀÎÇÏ±â
+			// ¿Â¶óÀÎ »ç¿ë ½Ã°£ ±âÁØÀ¸·Î ºÐ·ù
+			// A = 21 ~ , B = 11 ~ 20, C = 1 ~ 10
+			boolean time_checkA = Pattern.matches("\\d{21, }", input);	// ideal
+			boolean time_checkB = Pattern.matches("\\d{11, 20}", input);	// likely
+			boolean time_checkC = Pattern.matches("\\d{1,10}", input);	// defeat
 		}catch (Exception e) {
+			
+		}
+	}
+	public static String inputLocationString() {
+		try {
+			String input = INPUT.nextLine();
+			boolean location_check = Pattern.matches("\\^[°¡-ÆR{2, 4}]\\s[°¡-ÆR{2, 4}]$", input);
+		} catch (Exception e) {
 			
 		}
 	}
